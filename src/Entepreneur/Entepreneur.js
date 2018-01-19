f5373fb664368228a88751f6616a96af2264880c
Morton358 <vovankovalchuk358@gmail.com>
2018-01-19 20:28:44 +0100

    created card for output FuncFit value

--- src/Entepreneur/Entepreneur.js
+++ src/Entepreneur/Entepreneur.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 class Entepreneur extends Component {
 
   render() {
     return (
-      <div>
-        <p>Hello Entepreneur</p>
-      </div>
+      <Card>
+        <CardHeader
+          title="Results"
+          subtitle="Total cost: "
+          actAsExpander={true}
+          showExpandableButton={true}
+        />
+        <CardActions>
+          <FlatButton label="Action1" />
+          <FlatButton label="Action2" />
+        </CardActions>
+        <CardText expandable={true}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis
+          pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate
+          interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam
+          dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+      </Card>
     );
   }
 
